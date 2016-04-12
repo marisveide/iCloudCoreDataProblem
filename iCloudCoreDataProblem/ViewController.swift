cc77953bd84ef636d35705758f9de6a7b6a29368
marisveide <maris.veide@gmail.com>
2016-04-12 11:59:08 +0300

    Initial Commit

---
+++ iCloudCoreDataProblem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCloudCoreDataProblem
+//
+//  Created by Maris Veide on 12.04.2016.
+//  Copyright © 2016 ITissible. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
